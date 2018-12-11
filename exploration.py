aab969ec9fa61d00e8829d087789f9230df2fc16
Carter Carlson <carterbud1@gmail.com>
2018-12-11 13:58:24 -0600

    Finish data cleaning, just need to add some visuals

--- exploration.py
+++ exploration.py
@@ -4,13 +4,13 @@ import numpy as np
 import warnings
 import math
 
-movies = pd.read_csv('../Data/movies.csv')
+movies = pd.read_csv('../Data/movies.csv').sort_values('movieId')
 
 # Removing movies that have no genre listed
 movies = movies[movies['genres'] != '(no genres listed)']
 
 # Add year released as a feature
-movie_year = []
+movie_age = []
 for movie in movies['title']:
 	if '(199' in movie:
 		pos = movie.find('(199') + 1
@@ -19,23 +19,23 @@ for movie in movies['title']:
 	elif '(201' in movie:
 		pos = movie.find('(201') + 1
 	else:
-		movie_year.append(None)
+		movie_age.append(None)
 		continue
 
 	year = movie[pos:pos+4]
 
 	if 1995 > int(year):
-		movie_year.append(None)
+		movie_age.append(None)
 		continue
 
-	movie_year.append(year)
+	movie_age.append(2018 - int(year))
 
 # only keep movies released after 1995
-movies['year'] = movie_year
+movies['age'] = movie_age
 movies.dropna(inplace=True)
 
 # only keep movies that have ratings
-ratings = pd.read_csv('../Data/ratings.csv')[['rating', 'movieId']]
+ratings = pd.read_csv('../Data/ratings.csv')[['rating', 'movieId']].sort_values('movieId')
 movies = movies[movies['movieId'].isin(set(ratings['movieId']))]
 
 # Reduce ratings to only include movies of the clean movies dataframe
@@ -46,91 +46,48 @@ ratings['rating'] *= 2
 ratings['rating'] = ratings['rating'].astype(np.int8)
 ratings['movieId'] = ratings['movieId'].astype(np.int32)
 
-# before
-len(movies) # 27278
-len(ratings) # 20000263
-# after
-len(movies) # 15033
-len(ratings) # 10653093
+# # before
+# len(movies) # 27278
+# len(ratings) # 20000263
+# # after
+# len(movies) # 15033
+# len(ratings) # 10653093
 
-ratings_avg = ratings.groupby('movieId')[['movieId','rating']].mean().reset_index(drop=True)
-ratings_cnt = ratings.groupby('movieId').count()
+movies['rating average'] = ratings.groupby('movieId')['rating'].mean().tolist()
+movies['rating count'] = ratings.groupby('movieId')['rating'].count().tolist()
+movies['ratings per year'] = movies['rating count'] / movies['age']
 
-ratings_cnt.describe()
-# rating
-# count	15033.000000
-# mean	708.647176
-# std	2814.624231
-# min	1.000000
-# 25%	3.000000
-# 50%	18.000000
-# 75%	215.000000
-# max	53769.000000
-len(ratings_cnt[ratings_cnt['rating'] < 5]) / len(ratings_cnt) # 29.8% movies have less than 5 ratings
-len(ratings_cnt[ratings_cnt['rating'] == 1]) # 2054 movies have 1 rating
+# movies['rating count'].describe()
+# # count	15033.000000
+# # mean	708.647176
+# # std	2814.624231
+# # min	1.000000
+# # 25%	3.000000
+# # 50%	18.000000
+# # 75%	215.000000
+# # max	53769.000000
+# len(ratings_cnt[ratings_cnt['rating'] < 5]) / len(ratings_cnt) # 29.8% movies have less than 5 ratings
+# len(ratings_cnt[ratings_cnt['rating'] == 1]) # 2054 movies have 1 rating
 
-movies.head()
-ratings_avg
-
-ratings_dict = {ratings_avg['movieId'][i]:ratings_avg['rating'][i] for i in range(len(ratings_avg))}
-
-def find_movie_rating(movie):
-    return ratings_dict[movie]
-
-movies['average rating'] = list(map(find_movie_rating, movies['movieId']))
-
-# Sorting by average value
-movies.sort_values('average rating', inplace=True)
-movies = movies.reset_index(drop=True)
-
-print('Average movie rating:  {0:.2f}'.format(movies['average rating'].mean()))
-print('Median movie rating:   {0:.2f}'.format(movies['average rating'].median()))
-
-# Since our ratings are sorted, find how many records are in every 10%
-movies_10_pct = int(len(movies)/10)
-
-# Column of movie popularity
-movies['popularity'] = None
-bottom_20 = movies['popularity'].iloc[:movies_10_pct*2] = 1 # 'Worst'
-middle_20 = movies['popularity'].iloc[movies_10_pct*4:movies_10_pct*6] = 2 # 'OK'
-top_20 = movies['popularity'].iloc[movies_10_pct*8:] = 3 # 'Best'
-
-# Remove movies that are not in top, middle, or bottom 20%
-movies = movies.dropna().reset_index(drop=True)
-
-# Add year of movie
-movies['title'] = movies['title'].str.strip()
-def extract_year(title):
-    if title.find('(') < 0:
-        return int(title[:4])
-
-    return extract_year(title[title.find('(')+1:])
-
-# Column of movie age
-movies['year'] = [extract_year(movie) for movie in movies['title']]
-
-movies['age'] = None
-movies.loc[movies['year'] >= 1960, 'age'] = 1 # 'Old'
-movies.loc[movies['year'] >= 1970, 'age'] = 2 # 'Medium'
-movies.loc[movies['year'] >= 1990, 'age'] = 3 # 'New'
-
-movies = movies.dropna().reset_index(drop=True)
 
 # Make a column for each genre
 genre_groups = [movie.split('|') for movie in movies['genres']]
+
 genre_set = set()
 [[genre_set.add(genre) for genre in movie] for movie in genre_groups]
 genre_set = list(genre_set)
 
-df = pd.DataFrame(columns=[genre_set])
 
-df['average rating'] = movies['average rating']
-df['popularity'] = movies['popularity']
-df['age'] = movies['age']
+for genre in genre_set:
+	movies[genre] = 0
 
-df.fillna(0, inplace=True)
+# populate genres
+for i, genre in enumerate(genre_groups):
+	movies.loc[i, genre] = 1
 
-# populate dataframe
-for i in range(len(genre_groups)):
-    for genres in genre_groups[i]:
-        df.loc[i, genres] = 1
+movies.dropna(inplace=True)
+movies['rating average'] = movies['rating average'].astype(np.int8)
+
+y = movies['rating average']
+x = movies.drop(['rating average','movieId','title','genres'], axis=1)
+x = movies[genre_set]
